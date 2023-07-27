e13d962c54a6ebc1995bfe91d15c117dfae2819e
brucetravis <bruceambundo@gmail.com>
2023-07-27 02:04:53 -0700

    AUTHORS file

---
+++ handlearg1.c
@@ -0,0 +1,58 @@
+#include "shell.h"
+
+#define MAX_COMMAND_LENGTH 1024
+#define PROMPT "#cisfun$ "
+
+int main(void)
+{
+char command[MAX_COMMAND_LENGTH];
+char *args[MAX_COMMAND_LENGTH];
+int i;
+pid_t pid;
+int status;
+
+while (1)
+{
+printf("%s", PROMPT);
+        
+if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+{
+printf("\n");
+break;
+}
+
+command[strcspn(command, "\n")] = '\0';
+
+char *token;
+i = 0;
+
+token = strtok(command, " ");
+while (token != NULL)
+{
+args[i++] = token;
+token = strtok(NULL, " ");
+}
+args[i] = NULL;
+
+pid = fork();
+if (pid < 0)
+{
+perror("Fork failed");
+exit(EXIT_FAILURE);
+}
+else if (pid == 0)
+{
+if (execvp(args[0], args) == -1)
+{
+fprintf(stderr, "%s: Command not found\n", args[0]);
+exit(EXIT_FAILURE);
+}
+}
+else
+{
+waitpid(pid, &status, 0);
+}
+}
+
+return (EXIT_SUCCESS);
+}
