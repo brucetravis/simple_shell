d015c52696eb55e3c0b4f0f32be5b54d3121ab39
EngrBassey <basseyleads001@gmail.com>
2023-07-28 14:28:21 -0700

    betty checks err

--- buitin_handler.c
+++ buitin_handler.c
@@ -11,41 +11,33 @@ void execute_builtin(char **token, char **environ, char **lineptr)
 {
 	if (_strcmp(token[0], "exit") == 0)
 	{
-		 if (token[1] != NULL)
-                 {
-                         int exitStatus = atoi(token[1]);
-                         exit(exitStatus);
-                 }
-                 else
-                 {
-                 free(*lineptr);
-                exit(0);
-                 }
+		if (token[1] != NULL)
+		{
+			int exitStatus = atoi(token[1]);
+
+			exit(exitStatus);
+		{
+		else
+		{
+			free(*lineptr);
+			exit(0)
+		}
 	}
 	else if (_strcmp(token[0], "env") == 0)
-         {
-                 execut_env(environ);
-                 exit(0);
-         }
+	{
+		execut_env(environ);
+		exit(0);
+	}
 	else
 	{
 		fork_and_exec(token, environ, lineptr);
 	}
-
 }
 
-void execut_env(char **environ)
-{
-        char **ptr = environ;
-
-	while (*ptr != NULL)
-        {
-            write(STDOUT_FILENO, *ptr, _strlen(*ptr));
-            write(STDOUT_FILENO, "\n", 1);
-                ptr++;
-        }
-}
-
+/**
+  * execut_env - function that handles env
+  * @environ: the env
+  */
 void execut_env(char **environ)
 {
 	char **ptr = environ;
--- error_handler.c
+++ error_handler.c
@@ -38,3 +38,19 @@ int _strcmp(char *s1, char *s2)
 
 	return (0);
 }
+
+
+int _strncmp(const char *str1, const char *str2, size_t n)
+{
+	size_t i;
+
+    for (i = 0; i < n; i++) {
+        if (str1[i] != str2[i]) {
+            return (int)(unsigned char)str1[i] - (int)(unsigned char)str2[i];
+        }
+        if (str1[i] == '\0') {
+            break;
+        }
+    }
+    return 0;
+}
