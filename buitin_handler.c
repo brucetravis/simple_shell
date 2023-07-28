9dbee8beb779dd0011e8da4264e1e5da7f48d199
EngrBassey <basseyleads001@gmail.com>
2023-07-28 14:38:29 -0700

    fix

--- buitin_handler.c
+++ buitin_handler.c
@@ -16,24 +16,22 @@ void execute_builtin(char **token, char **environ, char **lineptr)
 			int exitStatus = atoi(token[1]);
 
 			exit(exitStatus);
-		{
+		}
 		else
 		{
 			free(*lineptr);
-			exit(0)
+			exit(0);
 		}
 	}
 	else if (_strcmp(token[0], "env") == 0)
 	{
 		execut_env(environ);
-		exit(0);
 	}
 	else
 	{
 		fork_and_exec(token, environ, lineptr);
 	}
 }
-
 /**
   * execut_env - function that handles env
   * @environ: the env
