252bf62ab34e4132d4b8a52663105091aaedda93
brucetravis <bruceambundo@gmail.com>
2023-07-28 04:13:03 -0700

    handling env command

--- buitin_handler.c
+++ buitin_handler.c
@@ -14,8 +14,25 @@ void execute_builtin(char **token, char **environ, char **lineptr)
 		free(*lineptr);
 		exit(0);
 	}
+	if (_strcmp(token[0], "env") == 0)
+         {
+                 execut_env(environ);
+                 exit(0);
+         }
 	else
 	{
 		fork_and_exec(token, environ, lineptr);
 	}
+}
+
+void execut_env(char **environ)
+{
+        char **ptr = environ;
+
+	while (*ptr != NULL)
+        {
+            write(STDOUT_FILENO, *ptr, _strlen(*ptr));
+            write(STDOUT_FILENO, "\n", 1);
+                ptr++;
+        }
 }
